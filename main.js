1556f81f824826af060a397915dc9443f46e71f9
ivantarabanovych <vanjokcitet334@gmail.com>
2024-07-03 21:05:02 +0300

    q4

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+let name = "Ivan";
+let age = 21;
+let isStudent = true;
+let heigth = 170;
+
 const person = {
-    name: "Ivan",
-    age: 21,
-    isStudent: true,
-    heigth: 170
+    name: name,
+    age: age,
+    isStudent: isStudent,
+    heigth: heigth
 };
 
 console.log(person);
