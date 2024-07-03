70a6f42921b89765ef38224eaa0a174ecd66505c
ivantarabanovych <vanjokcitet334@gmail.com>
2024-07-03 18:26:02 +0300

    q3

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
-let number = 21;
-let stringNumber = "двадцять один"
-console.log(number == stringNumber);
-console.log(number === stringNumber);
-
-let boolValue = true;
-let nullValue = null;
-
-console.log(boolValue == nullValue);
-console.log(nullValue === boolValue);
-
-let zero = 0;
-
-console.log(nullValue == zero);
-console.log(nullValue === zero);
-
-let falseValue = false;
-
-console.log(falseValue == zero);
-console.log(falseValue === zero);
-
-let stringZero = "0";
-
-console.log(stringZero == zero);
-console.log(stringZero === zero);
-
-let undefindeValue = undefined;
-
-console.log(undefindeValue == nullValue);
-console.log(undefindeValue === nullValue);
-
+let name = "Ivan"
+let age = 21;
+let description = (`Мене звати ${name}, мій вік ${age}`);
+console.log(description);
+console.log(typeof description);
